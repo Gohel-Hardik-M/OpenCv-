4831149ec5f95456dcc9bc2a68146883b1cd818c
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-07 06:32:06 +0530

    Update color_spaces.py

--- code/color_spaces.py
+++ code/color_spaces.py
@@ -1,1 +1,24 @@
 import cv2 as cv
+
+img = cv.imread('Photos/war.jpg')
+cv.imshow('Orignal Image',img)
+
+#COnverting original image in grayscale image 
+gray = cv.cvtcolor(img,cv.COLOR_BGR2GRAY)
+cv.imshow('Gray',gray)
+
+# COnverting to HSV image 
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+cv.imshow('HSV ',hsv)
+
+# COnverting to L*A*B img
+lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
+cv.imshow('LAB',lab)
+
+#Convering to RGB Image
+rgb = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+cv.imshow('RGB',rgb)
+
+cv.waitkey(0)
+
+
