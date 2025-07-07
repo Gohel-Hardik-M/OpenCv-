d7dbd3a8c57b827a4472dab4854fd8cbcaf96df5
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-07 06:49:35 +0530

    Update color_channels.py

--- code/color_channels.py
+++ code/color_channels.py
@@ -1,1 +1,18 @@
-import cv2 as cvSS
+ import cv2 as cv
+
+img = cv.imread('Photos/war.jpg')
+cv.imshow('Original Image',img)
+
+b,g,r= cv.split(img)
+
+cv.imshow('Blue',b)
+cv.imshow('Green',g)
+cv.imshow('Red',r)
+
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+
+cv.waitkey(0)
