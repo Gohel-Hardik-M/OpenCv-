6ae26846c6b48824071992a5ea97a8baee97a143
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-07 06:51:27 +0530

    Update color_channels.py

--- code/color_channels.py
+++ code/color_channels.py
@@ -1,15 +1,20 @@
- import cv2 as cv
+# In this block of code we are gonna split the image into BGR formate like we will get the Red scale , Blue Scale , And Green Scale Images
+
+
+import cv2 as cv
+
 
 img = cv.imread('Photos/war.jpg')
 cv.imshow('Original Image',img)
 
 b,g,r= cv.split(img)
 
-cv.imshow('Blue',b)
-cv.imshow('Green',g)
-cv.imshow('Red',r)
+cv.imshow('Blue',b)   #Blue Scale image
+cv.imshow('Green',g)  #Green scale image
+cv.imshow('Red',r)    #Red scale image
 
-print(img.shape)
+# shapes of every images
+print(img.shape)   
 print(b.shape)
 print(g.shape)
 print(r.shape)
