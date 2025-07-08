e6e1a6babbf092f2941063efb4df257337c2d9b9
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-08 07:44:27 +0530

    Update blurring_techniques.py

--- code/blurring_techniques.py
+++ code/blurring_techniques.py
@@ -3,6 +3,15 @@ import cv2 as cv
 img = cv.imread('Photos/war.jpg')
 cv.imshow('Original Image',img)
 
+
+# 1st Method --> Avaraging 
+avarage = cv.blur(img,(3,3))   # (3,3) -->is the kernel size --> Higher The kerner size more the blurr effect
+cv.imshow(" Avarage Blurr Image",avarage)
+
+
+# 2nd Method --> Gaussian Blur
+
+
 cv.waitKey(0)
 
 
