fdf8f227e5f31f6262e32958942251b1ce8c2309
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-09 07:45:14 +0530

    Update blurring_techniques.py

--- code/blurring_techniques.py
+++ code/blurring_techniques.py
@@ -10,7 +10,15 @@ cv.imshow(" Avarage Blurr Image",avarage)
 
 
 # 2nd Method --> Gaussian Blur
+gaussian = cv.GaussianBlur(img , (7,7),0)
+cv.imshow("gaussian Blur",gaussian)
 
+# 3rd Method --> Median Blur
+median = cv.medianBlur(img,3)
+cv.imshow("Median Blur",median)
+
+# 4th method -->Bilateral 
+bilatera; = cv.bilateralFilter(img,5,15, 15 )
 
 cv.waitKey(0)
 
