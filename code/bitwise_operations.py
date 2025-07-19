92e66edf0f085577aeea95490ac9029cbe65132e
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-19 12:40:18 +0530

    Update bitwise_operations.py

--- code/bitwise_operations.py
+++ code/bitwise_operations.py
@@ -6,4 +6,4 @@ rectangle = cv.rectangle(blank.copy(),(30,30),(370,370),255,-1)
 circle = cv.circle(blank.copy(),(200,200),200,255,-1)
 
 cv.imshow('Rectangle',rectangle)
-cv.imshow('Circle',circle)
+cv.imshow('Circle',circle)   
