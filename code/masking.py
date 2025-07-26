5ba80f716745cb8fdbae6499231377175f18c0a6
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-26 12:58:15 +0530

    Update masking.py

--- code/masking.py
+++ code/masking.py
@@ -9,7 +9,7 @@ cv.imshow('Blank Image',blank)
 
 mask = cv.circle(blank, (img.shape[1]//2,img.shape[0]//2),100,255,-1)
 cv.imshow('Mask',mask)
-
+ 
 masked = cv.bitwise_and(img,img,mask = mask)
 cv.imshow('Masked Image', masked)
 
