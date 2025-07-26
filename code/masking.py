bbd0139982e8abe84787bbfe38685af378bb0541
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-26 12:25:08 +0530

    Update masking.py

--- code/masking.py
+++ code/masking.py
@@ -4,4 +4,10 @@ img = cv.imread('Photos/war.png')
 cv.imshow('Img',img)
 
 
-blank =  
+blank =  np.zeros(img.shape[:2],dtype='uint8')
+cv.imshow('Blank Image',blank)
+
+masked = cv.bitwise_and(img,img,mask = mask)
+cv.imshow('Masked Image', masked)
+
+cv.waitkey(0)
