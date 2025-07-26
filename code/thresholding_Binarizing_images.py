4ff4731b5a28f426f0f93c085ec466ec389b4490
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-26 15:34:33 +0530

    Update thresholding_Binarizing_images.py

--- code/thresholding_Binarizing_images.py
+++ code/thresholding_Binarizing_images.py
@@ -3,5 +3,11 @@ import cv2 as cv
 img = cv.imread('Photos/war.jpg')
 cv.imshow('Original Image',img)
 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('GRAY IMAGE',gray)
+
+
+#Simple THresholding 
+threshold, thresh = cv.threshold(gray,150,255, cv.THRESH_BINARY)
 
 cv.waitKey(0)
