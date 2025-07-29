9f5b74de4a963832f8e14d73fd882377ca68ab72
GOHEL HARDIK <hardikmg21@gmail.com>
2025-07-29 19:07:10 +0530

    Update Edge_Detection_ADVANCED.py

--- code/Edge_Detection_ADVANCED.py
+++ code/Edge_Detection_ADVANCED.py
@@ -1,1 +1,8 @@
 import cv2 as cv
+import numpy as np 
+
+img = cv.imread('Photos/war.jpg')
+cv.imshow('Original Image',img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray Image',gray)
