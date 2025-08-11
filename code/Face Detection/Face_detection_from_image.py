c2d492b29e42a8b09e39c25b17b6e6b529c10da1
GOHEL HARDIK <hardikmg21@gmail.com>
2025-08-11 19:55:42 +0530

    Update Face_detection_from_image.py

--- code/Face Detection/Face_detection_from_image.py
+++ code/Face Detection/Face_detection_from_image.py
@@ -1,1 +1,8 @@
 import cv2 as cv
+import numpy as np 
+
+# Classifier
+haar_cascase = cv.CascadeClassifier('haar_face.xml')
+
+# to detect the face in any image firstly we need to convert the original image into a GRAY scale image
+img = cv.imread('')
